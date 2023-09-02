1464d8e8c9d91e0939d335593d8ce05f84e5cd86
Niall <niall.j.miller@gmail.com>
2023-09-02 11:20:04 +0100

    fixed

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(StochiStats
+setup(name = "StochiStats",
     version="0.1",
     packages=find_packages(),
     install_requires=['numpy','scipy','scikit-learn'],
